315fa00cef2fb36e66466476a0550032d0347c8c
alan <alanlopes4@gmail.com>
2015-04-10 17:33:32 -0300

    Criação das entidades

---
+++ youteacherweb/main/java/br/com/youteacherweb/entidades/Usuario.java
@@ -0,0 +1,80 @@
+package br.com.youteacherweb.entidades;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String nome;
+	
+	private String email;
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+	
+	private String login;
+	
+	private String senha;
+	
+	private String adm;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+}
