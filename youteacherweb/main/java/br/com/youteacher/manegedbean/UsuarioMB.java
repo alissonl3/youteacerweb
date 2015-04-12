f172b390f1884ab1d0a886f5c8882e8b9f758b3b
alissonl3 <alissonlopes3@gmail.com>
2015-04-12 16:39:48 -0300

    Novo

--- youteacherweb/main/java/br/com/youteacher/manegedbean/UsuarioMB.java
+++ youteacherweb/main/java/br/com/youteacher/manegedbean/UsuarioMB.java
@@ -5,10 +5,13 @@ package br.com.youteacher.manegedbean;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 
 
 
+
+import javax.faces.context.FacesContext;
 
 import br.com.youteacher.banco.BancoDAO;
 import br.com.youteacher.viewbean.UsuarioViewBean;
@@ -17,7 +20,7 @@ import br.com.youteacher.viewbean.UsuarioViewBean;
 public class UsuarioMB implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private BancoDAO dao = new BancoDAO();
+	private BancoDAO dao ;
 
 	private UsuarioViewBean viewBean;
 
@@ -28,7 +31,7 @@ public class UsuarioMB implements Serializable {
 	@PostConstruct
 	private void initPage(){
 		
-		
+		dao = new BancoDAO();
 		viewBean = new UsuarioViewBean();
 		
 	}
@@ -36,8 +39,49 @@ public class UsuarioMB implements Serializable {
 	//INSERIR UM NOVO ALUNO
 	public void inserir(){
 		
+		try{
+			
+			dao.inserir(viewBean.getUsuario());
+			mostraMenssagem("SUCESSO", "Usuario inserido com sucesso.");
+			
+			
+		}catch(Exception e){
+			
+			mostraMenssagem("ERRO", "Houve um erro ao tentar inserir usuario.");
+			System.out.println("Erro " + e);
+		}
+		
 		
 	}
+	
+	//INSERIR UM NOVO ALUNO
+		public void deletar(){
+			
+			try{
+				
+				dao.deletar(viewBean.getUsuario());
+				mostraMenssagem("SUCESSO", "Usuario deletado com sucesso.");
+				
+				
+			}catch(Exception e){
+				
+				mostraMenssagem("ERRO", "Houve um erro ao tentar deletar usuario.");
+				System.out.println("Erro " + e);
+			}
+			
+			
+		}
+	
+	
+	//MOSTRAR MENSSAGEM DE NOTFICAÇÃO
+	 public void mostraMenssagem(String titulo, String menssagem){
+	        
+	        FacesContext fc = FacesContext.getCurrentInstance();
+	        FacesMessage men = new FacesMessage(titulo, menssagem);
+	        fc.addMessage(null, men);
+	    
+	    
+	    }
 
 	public UsuarioViewBean getViewBean() {
 		return viewBean;
